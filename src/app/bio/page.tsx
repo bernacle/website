76a091eb11ebccda7675d00c0e3e08aff2e7009a
Bruno Simplicio <bruno@notabene.id>
2024-01-22 20:26:25 -0400

    fix: Website to be published

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,9 +1,88 @@
-export default async function CV() {
+import Link from 'next/link'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Metadata } from 'next'
+import { Section } from '@/components/ui/section'
+import { GlobeIcon, MailIcon, PhoneIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { RESUME_DATA } from '@/data/'
+import Tabs from '@/components/ui/tabs'
+
+export default async function Bio() {
   return (
-    <div className="flex justify-center height-screen-helper">
-      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80">
-        <text>This is where my bio will be</text>
+    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12">
+      <section className="mx-auto w-full max-w-2xl space-y-8 bg-black print:space-y-6">
+        <div className="flex flex-col items-center justify-between">
+          <h1 className="text-white text-5xl font-extrabold  text-center">
+            Hi, I am <span className="text-green-400">Bruno</span>
+          </h1>
+          <div className="flex-col py-20 text-justify">
+            <p className="text-white text-base p-4">
+              With a solid <span className="text-green-400">decade</span> in the
+              tech trenches, I am a committed software engineer passionate about
+              building robust and scalable applications
+            </p>
+            <p className="text-white text-base p-4">
+              {`I'm`} fluent in{' '}
+              <span className="text-green-400">Typescript</span> and have a
+              talent for crafting code {`that's`} both powerful and easy to
+              manage
+            </p>
+            <p className="text-white text-base p-4">
+              Eager to ride the wave of new technologies and level up my skills,
+              {`I'm`} always up for a new challenge.
+            </p>
+            <p className="text-white text-base p-4">
+              If you want to know a little more about my professional
+              experience, just check it out my{' '}
+              <Link href="/cv">
+                <span className="text-green-400">CV</span>
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className="flex gap-x-4 pt-1 font-mono text-sm text-muted-foreground print:hidden justify-center">
+        {RESUME_DATA.contact.email ? (
+          <Button
+            className="size-14 border-green-400 hover:text-green-400"
+            variant="outline"
+            size="icon"
+            asChild
+          >
+            <a href={`mailto:${RESUME_DATA.contact.email}`}>
+              <MailIcon className="size-4" />
+            </a>
+          </Button>
+        ) : null}
+        {RESUME_DATA.contact.tel ? (
+          <Button
+            className="size-14 border-green-400 hover:text-green-400"
+            variant="outline"
+            size="icon"
+            asChild
+          >
+            <a href={`tel:${RESUME_DATA.contact.tel}`}>
+              <PhoneIcon className="size-4" />
+            </a>
+          </Button>
+        ) : null}
+        {RESUME_DATA.contact.social.map((social) => (
+          <Button
+            key={social.name}
+            className="size-14 border-green-400 hover:text-green-400"
+            variant="outline"
+            size="icon"
+            asChild
+          >
+            <a href={social.url}>
+              <social.icon className="size-4" />
+            </a>
+          </Button>
+        ))}
       </div>
-    </div>
+    </main>
   )
 }
